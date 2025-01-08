5021a53bfa1272fd6e1a5d650163bff33c8dad03
Vlad Harkusha <sivari888@gmail.com>
2025-01-08 18:28:41 +0200

    upd solution hw2-task1

--- src/services/todo-api.service.ts
+++ src/services/todo-api.service.ts
@@ -1,6 +1,8 @@
+import {ITodo} from "../models/ITodo.ts";
+
 const baseUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-export const getTodos = async () => {
+export const getTodos = async (): Promise<ITodo[]> => {
     const response = await fetch(baseUrl);
 
     return response.json();
