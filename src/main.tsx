4788f7aa6523b629c61c0e6867d52329fc9d7a53
Vlad Harkusha <sivari888@gmail.com>
2025-01-15 17:49:43 +0200

    template with base components

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {HomePage} from "./pages/home-page/HomePage.tsx";
 import {CarsPage} from "./pages/cars-page/CarsPage.tsx";
-import {NewCarPage} from "./pages/new-car-page/NewCarPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
@@ -12,7 +11,6 @@ createRoot(document.getElementById('root')!).render(
             <Route path='' element={ <App /> } >
                 <Route index element={ <HomePage /> } />
                 <Route path='/cars' element={ <CarsPage /> } />
-                <Route path='/cars/add-new-car' element={ <NewCarPage /> } />
             </Route>
         </Routes>
     </BrowserRouter>
--- src/pages/cars-page/CarsPage.tsx
+++ src/pages/cars-page/CarsPage.tsx
@@ -1,14 +1,9 @@
 import {FC} from "react";
-import {Link} from "react-router-dom";
-import {CarsList} from "../../components/cars-list/CarsList.tsx";
 
 export const CarsPage: FC = () => {
     return (
         <>
-            <Link to={'/cars/add-new-car'} className='bg-yellow-300 inline-block p-2 border-yellow-800 border-2 my-2'>
-                Add new car
-            </Link>
-            <CarsList />
+            Cars page
         </>
     );
 };
