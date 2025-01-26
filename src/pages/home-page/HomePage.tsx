1c3da806755c6a4247eacbc7e882bbe9b7744eb5
Vlad Harkusha <sivari888@gmail.com>
2025-01-26 20:05:30 +0200

    homework7 task 1 completed

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -2,7 +2,7 @@ import {FC} from "react";
 
 export const HomePage: FC = () => {
     return (
-        <h1 className='text-3xl font-bold p-5'>
+        <h1 className='text-3xl font-bold p-5 text-blue-800'>
             Home Page
         </h1>
     );
